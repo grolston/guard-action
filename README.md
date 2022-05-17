69aa726f2fb5aa450b76726a728a32d27f808c28
George Rolston <grolston@outlook.com>
2022-05-16 21:24:08 -0600

    init

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # ShiftLeft
+
+
+A simple `GitHub Action` for AWS CloudFormation static code analysis to improve infrastructure-as-code security using AWS cfn-guard.
+
+***The Action does not require AWS credentials!***
+
+cfn-security supports the following linting and security tools:
+
+## Inputs
+
+### `data_directory`
+
+The directory of the repo to scan the cloudformation templates.
+
+### `rule_set`
+
+The scanner used to run security test. Options are `FedRAMP-Low`, `FedRAMP-Moderate`, `NIST800-53Rev4`, `NIST800-53Rev4`, `PCI-DSS-3-2-1`
+
+## Usage
+
+To get started simply add a workflow `.yml` file (name it whatever you would like) to your `.github/workflows` folder. [Refer to the documentation on workflow YAML syntax here.](https://help.github.com/en/articles/workflow-syntax-for-github-actions).
+
+
+
+## Example cfn-lint Test
+
+The following example tests CloudFormation with cfn-lint:
+
+```yaml
+name: ShiftLeft
+
+on: [push]
+
+jobs:
+  ## Guard rule set FedRAMP-Moderate
+  sast-cfn-lint:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v2
+    - uses: grolston/ShiftLeft@main
+      with:
+        data_directory: './cloudformation/' ## change to your template directory
+        rule_set: "FedRAMP-Moderate"
+```
+
+## License
+
+This project is distributed under the [Apache 2.0](LICENSE.md).
---
+++ entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+if [ -z "$INPUT_DATA_DIRECTORY" ]; then
+  echo "Environment variable DATA_DIRECTORY is not set. Quitting."
+  exit 1
+fi
+
+if [ ! -d "$INPUT_DATA_DIRECTORY" ]; then
+  echo "${INPUT_DATA_DIRECTORY} path not found. Quitting."
+  exit 1
+fi
+
+case $INPUT_RULE_SET in
+
+  "FedRAMP-Moderate")
+    echo -n "scanning with guard rule set RedRAMP-Moderate"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules ./${INPUT_RULE_SET}.guard"
+    ;;
+
+  "FedRAMP-Low")
+    echo -n "...scanning with guard rule set FedRAMP-Low"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules ./${INPUT_RULE_SET}.guard"
+    ;;
+
+  "NIST800-53Rev4")
+    echo -n "...scanning with guard rule set NIST800-53Rev4"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules ./${INPUT_RULE_SET}.guard"
+    ;;
+
+  "NIST800-53Rev5")
+    echo -n "...scanning with guard rule set NIST800-53Rev5"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules ./${INPUT_RULE_SET}.guard"
+    ;;
+
+  "PCI-DSS-3-2-1")
+    echo -n "...scanning with only guard rule set PCI-DSS-3-2-1"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules ./${INPUT_RULE_SET}.guard"
+    ;;
+
+  *)
+    echo -n "Environment variable RULE_SET is not set to an allowed option...Quitting."
+    exit 1
+    ;;
+esac
