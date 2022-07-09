8e0a35c9428a8709d4e9df95aa3dca7cd43043b0
George Rolston <grolston@amazon.com>
2022-07-09 15:48:53 -0600

    init 1.0.0

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# ShiftLeft
-
+# Guard-Action
 
 A simple `GitHub Action` for AWS CloudFormation static code analysis to improve infrastructure-as-code security using AWS cfn-guard.
 
 ***The Action does not require AWS credentials!***
 
+It gets better:
+**Guard Rules in this github action can be suppressed at the resource-level!** See [Guard Rule Suppression](#guard-rule-suppression) to learn how to leverage in your code.
 
 ## Inputs
 
@@ -16,11 +17,55 @@ The directory of the repo to scan the cloudformation templates.
 
 The AWS Guard rule set used to run security test. Options are:
 
- * `FedRAMP-Low`
- * `FedRAMP-Moderate`
- * `NIST800-53Rev4`
- * `NIST800-53Rev4`
- * `PCI-DSS-3-2-1`
+1. ABS-CCIGv2-Material
+2. ABS-CCIGv2-Standard
+3. acsc-essential-8
+4. acsc-ism
+5. apra-cpg-234
+6. bnm-rmit
+7. cis-aws-benchmark-level-1
+8. cis-aws-benchmark-level-2
+9. cis-critical-security-controls-v8-ig1
+10. cis-critical-security-controls-v8-ig2
+11. cis-critical-security-controls-v8-ig3
+12. cis-top-20
+13. cisa-ce
+14. cmmc-level-1
+15. cmmc-level-2
+16. cmmc-level-3
+17. cmmc-level-4
+18. cmmc-level-5
+19. enisa-cybersecurity-guide-for-smes
+20. ens-high
+21. ens-low
+22. ens-medium
+23. FDA-21CFR-Part-11
+24. FedRAMP-Low
+25. FedRAMP-Moderate
+26. ffiec
+27. hipaa-security
+28. K-ISMS
+29. mas-notice-655
+30. mas-trmg
+31. nbc-trmg
+32. ncsc-cafv3
+33. ncsc
+34. nerc
+35. nist-1800-25
+36. nist-800-171
+37. nist-800-172
+38. nist-800-181
+39. NIST800-53Rev4
+40. NIST800-53Rev5
+41. nist-csf
+42. nist-privacy-framework
+43. nzism
+44. PCI-DSS-3-2-1
+45. rbi-bcsf-ucb
+46. rbi-md-itf
+47. us-nydfs
+48. wa-Reliability-Pillar
+49. wa-Security-Pillar
 
 ## Usage
 
@@ -41,10 +86,44 @@ jobs:
     runs-on: ubuntu-latest
     steps:
     - uses: actions/checkout@v2
-    - uses: grolston/ShiftLeft@main
+    - uses: grolston/guard-action@main
       with:
         data_directory: './cloudformation/' ## change to your template directory
         rule_set: "FedRAMP-Moderate"
+```
+
+## Guard Rule Suppression
+
+The rules build in `guard-action` support resource-level rule suppression. Suppressed Rules are marked as SKIP in the validation process. To suppress a rule add in to the resource metadata the following `Metadata`:
+
+**YAML:**
+Suppressing rule `EC2_INSTANCE_PROFILE_ATTACHED`
+```yaml
+Resources:
+  ExampleEC2:
+    Type: AWS::EC2::Instance
+    Metadata:
+      guard:
+        SuppressedRules:
+        - EC2_INSTANCE_PROFILE_ATTACHED
+```
+
+**JSON:**
+Suppressing rule `ELASTICSEARCH_ENCRYPTED_AT_REST`
+```json
+"Resources": {
+  "ElasticsearchDomain": {
+    "Type": "AWS::Elasticsearch::Domain",
+    "Metadata": {
+      "guard": {
+        "SuppressedRules": ["ELASTICSEARCH_ENCRYPTED_AT_REST"]
+      }
+    },
+    "Properties": {
+      "DomainName": "test"
+    }
+  }
+}
 ```
 
 ## License
--- entrypoint.sh
+++ entrypoint.sh
@@ -12,33 +12,206 @@ fi
 
 case $INPUT_RULE_SET in
 
-  "FedRAMP-Moderate")
-    echo -n "scanning with guard rule set RedRAMP-Moderate"
+  "ABS-CCIGv2-Material")
+    echo -n "...scanning with only guard rule set ABS-CCIGv2-Material"
     sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
     ;;
-
+  "ABS-CCIGv2-Standard")
+    echo -n "...scanning with only guard rule set ABS-CCIGv2-Standard"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "acsc-essential-8")
+    echo -n "...scanning with only guard rule set acsc-essential-8"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "acsc-ism")
+    echo -n "...scanning with only guard rule set acsc-ism"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "apra-cpg-234")
+    echo -n "...scanning with only guard rule set apra-cpg-234"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "bnm-rmit")
+    echo -n "...scanning with only guard rule set bnm-rmit"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "cis-aws-benchmark-level-1")
+    echo -n "...scanning with only guard rule set cis-aws-benchmark-level-1"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "cis-aws-benchmark-level-2")
+    echo -n "...scanning with only guard rule set cis-aws-benchmark-level-2"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "cis-critical-security-controls-v8-ig1")
+    echo -n "...scanning with only guard rule set cis-critical-security-controls-v8-ig1"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "cis-critical-security-controls-v8-ig2")
+    echo -n "...scanning with only guard rule set cis-critical-security-controls-v8-ig2"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "cis-critical-security-controls-v8-ig3")
+    echo -n "...scanning with only guard rule set cis-critical-security-controls-v8-ig3"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "cis-top-20")
+    echo -n "...scanning with only guard rule set cis-top-20"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "cisa-ce")
+    echo -n "...scanning with only guard rule set cisa-ce"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "cmmc-level-1")
+    echo -n "...scanning with only guard rule set cmmc-level-1"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "cmmc-level-2")
+    echo -n "...scanning with only guard rule set cmmc-level-2"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "cmmc-level-3")
+    echo -n "...scanning with only guard rule set cmmc-level-3"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "cmmc-level-4")
+    echo -n "...scanning with only guard rule set cmmc-level-4"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "cmmc-level-5")
+    echo -n "...scanning with only guard rule set cmmc-level-5"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "enisa-cybersecurity-guide-for-smes")
+    echo -n "...scanning with only guard rule set enisa-cybersecurity-guide-for-smes"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "ens-high")
+    echo -n "...scanning with only guard rule set ens-high"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "ens-low")
+    echo -n "...scanning with only guard rule set ens-low"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "ens-medium")
+    echo -n "...scanning with only guard rule set ens-medium"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "FDA-21CFR-Part-11")
+    echo -n "...scanning with only guard rule set FDA-21CFR-Part-11"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
   "FedRAMP-Low")
     echo -n "...scanning with guard rule set FedRAMP-Low"
     sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
     ;;
-
+  "FedRAMP-Moderate")
+    echo -n "scanning with guard rule set FedRAMP-Moderate"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "ffiec")
+    echo -n "scanning with guard rule set ffiec"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "hipaa-security")
+    echo -n "scanning with guard rule set hipaa-security"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "K-ISMS")
+    echo -n "scanning with guard rule set K-ISMS"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "mas-notice-655")
+    echo -n "scanning with guard rule set mas-notice-655"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "mas-trmg")
+    echo -n "scanning with guard rule set mas-trmg"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "nbc-trmg")
+    echo -n "scanning with guard rule set nbc-trmg"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "ncsc-cafv3")
+    echo -n "scanning with guard rule set ncsc-cafv3"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "ncsc")
+    echo -n "scanning with guard rule set ncsc"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "nerc")
+    echo -n "scanning with guard rule set nerc"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "nist-1800-25")
+    echo -n "scanning with guard rule set nist-1800-25"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "nist-800-171")
+    echo -n "scanning with guard rule set nist-800-171"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "nist-800-172")
+    echo -n "scanning with guard rule set nist-800-172"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "nist-800-181")
+    echo -n "scanning with guard rule set nist-800-181"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
   "NIST800-53Rev4")
     echo -n "...scanning with guard rule set NIST800-53Rev4"
     sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
     ;;
-
   "NIST800-53Rev5")
     echo -n "...scanning with guard rule set NIST800-53Rev5"
     sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
     ;;
-
+  "nist-csf")
+    echo -n "scanning with guard rule set nist-csf"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "nist-privacy-framework")
+    echo -n "scanning with guard rule set nist-privacy-framework"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "nzism")
+    echo -n "...scanning with only guard rule set nzism"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
   "PCI-DSS-3-2-1")
     echo -n "...scanning with only guard rule set PCI-DSS-3-2-1"
     sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
     ;;
-
+  "rbi-bcsf-ucb")
+    echo -n "scanning with guard rule set rbi-bcsf-ucb"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "rbi-md-itf")
+    echo -n "scanning with guard rule set rbi-md-itf"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "us-nydfs")
+    echo -n "scanning with guard rule set us-nydfs"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "wa-Reliability-Pillar")
+    echo -n "scanning with guard rule set wa-Reliability-Pillar"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
+  "wa-Security-Pillar")
+    echo -n "scanning with guard rule set wa-Security-Pillar"
+    sh -c "cfn-guard validate --data ${INPUT_DATA_DIRECTORY} --rules /${INPUT_RULE_SET}.guard --show-summary fail -p"
+    ;;
   *)
     echo -n "Environment variable RULE_SET is not set to an allowed option...Quitting."
     exit 1
     ;;
+
+
 esac
